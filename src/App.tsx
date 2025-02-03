db43f960c6d65e7bb6974a6a77f30d944adb3a8c
ejPark43 <pej22200310@handong.ac.kr>
2025-02-03 15:59:37 +0900

    chores: 구현 기능 별 파일 분리, 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-
+import { BrowserRouter, Route, Router, Routes } from "react-router";
 import "./App.css";
-import Chessboard from "./components/Chessboard/Chessboard.tsx";
+import Chessboard from "./ChessboardPage/components/Chessboard.tsx";
+import Home from "./Home.tsx";
 
 function App() {
   return (
-    <div id="app">
-      <Chessboard />
+    <div id="home-container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chessboard" element={<Chessboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
